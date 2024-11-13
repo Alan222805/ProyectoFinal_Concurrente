39b16d3457111c8e2fd6218b324072b5e06ce196
EnriqueVal09 <quiquechavezrm@gmail.com>
2024-11-13 09:21:07 -0600

    Agregue ayuda y carpetad de los programas

--- proyecto_final.py
+++ proyecto_final.py
@@ -1,20 +1,10 @@
+import subprocess
 import tkinter as tk
 from tkinter import Menu
+import os
 
 # Lista de apartados principales del menú
 apartados = ["Hilos", "Sockets", "Semaforos", "Patrones", "Documentacion", "Ayuda", "Salir"]
-
-# Crear la ventana principal
-root = tk.Tk()
-root.title("Interfaz con Menú")  # Título de la ventana
-root.geometry("1920x1080")  # Tamaño de la ventana
-
-# Agregar un título en la ventana
-label_title = tk.Label(root, text="Bienvenido a la Interfaz", font=("Arial", 40))
-label_title.pack(pady=10)
-
-# Crear el menú principal
-menu_bar = Menu(root)
 
 # Subapartados para cada apartado del menú
 subapartados = {
@@ -49,25 +39,45 @@ subapartados = {
     ]
 }
 
-# Crear los apartados en el menú
-for apartado in apartados:
-    menu = Menu(menu_bar, tearoff=0)
-    
-    # Añadir subapartados si existen para el apartado actual
+# Función para abrir el una ventana flotante
+def abrir_ventana(archivo, carpeta=None):
+    ruta = os.path.join("menus", carpeta or "", f"{archivo}.py")
+    subprocess.Popen(["python", ruta])
+
+
+# Función para crear un menú con subapartados
+def crear_menu(apartado, root):
+    menu = Menu(root, tearoff=0)
+
     if apartado in subapartados:
         for subapartado in subapartados[apartado]:
-            menu.add_command(label=subapartado)
-        menu.add_separator()  # Separador entre el último subapartado y el final del menú
+            menu.add_command(label=subapartado, command=lambda: abrir_ventana(subapartado.lower().replace(" ", "_"), apartado.lower())) 
+        menu.add_separator()
     
-    # Añadir opción de Salir si el apartado es "Salir"
     if apartado == "Salir":
         menu.add_command(label="Salir", command=root.quit)
 
-    # Añadir el menú al menú principal
-    menu_bar.add_cascade(label=apartado, menu=menu)
+    if apartado == "Ayuda":
+        menu.add_command(label="Abrir Ayuda", command=lambda: abrir_ventana("ayuda"))
+    return menu
 
-# Configurar la barra de menú en la ventana
-root.config(menu=menu_bar)
+# Función para configurar la ventana principal
+def configurar_ventana(root):
+    root.title("Interfaz con Menú")
+    root.geometry("1920x1080")
+    label_title = tk.Label(root, text="Proyecto final\nProgramacion concurrente", font=("Arial", 40))
+    label_title.pack(pady=10)
 
-# Iniciar el bucle principal de la aplicación
-root.mainloop()
+# Función principal para crear la interfaz
+def main():
+    root = tk.Tk()
+    configurar_ventana(root)
+    menu_bar = Menu(root)
+    for apartado in apartados:
+        menu = crear_menu(apartado, root)
+        menu_bar.add_cascade(label=apartado, menu=menu)
+    root.config(menu=menu_bar)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
